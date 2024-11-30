5bcb8624711e4a60474992c840d8815e7e6cec79
Cyn-37 <m.rahimy38a1@gmail.com>
2024-11-30 21:12:09 +0330

    initial commit

---
+++ S07 First CLI Project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="showDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone: </strong>{{ friend.phone }}</li>
+        <li><strong>mail: </strong>{{ friend.email }}</li>
+    </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() { 
+        return {
+            detailsAreVisible: false,
+            friend: {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 45678 90',
+                email: 'manuel@localhost.com',
+            },
+        };
+    },
+
+    methods: {
+        showDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    },
+    
+};
+</script>
