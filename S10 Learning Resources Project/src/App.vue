2333e6ee5599f09ef3d7d768d6ff2a674b29d38f
Cyn-37 <m.rahimy38a1@gmail.com>
2024-12-12 19:58:16 +0330

    initial commit

---
+++ S10 Learning Resources Project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <the-header title="Remember Me"></the-header>
+  <navigate-tab></navigate-tab>
+</template>
+
+<script>
+
+import TheHeader from './components/layouts/TheHeader.vue';
+import NavigateTab from './components/layouts/NavigateTab.vue';
+
+export default {
+  components: {
+     TheHeader, NavigateTab
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
